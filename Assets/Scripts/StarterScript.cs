3e71498c2d6b621b787d09f325bcb618911990a5
Unknown <liamjkelly11@gmail.com>
2017-07-18 10:21:50 -0400

    Submitted Version
    Added music, messages for when it starts and ends. Still have to work on making machine start on a click.

--- Assets/Scripts/StarterScript.cs
+++ Assets/Scripts/StarterScript.cs
@@ -6,16 +6,14 @@ using UnityEngine.UI;
 public class StarterScript : MonoBehaviour {
 
 	public Text myText;
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public AudioSource mySource;
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Space)) {
 			myText.text = "";
 			//GetComponent<BoxCollider>().gameObject.SetActive(true);
+			mySource.Play ();
 		}
 	}
 }
--- Assets/Scripts/TriggerEnd.cs
+++ Assets/Scripts/TriggerEnd.cs
@@ -1,13 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TriggerEnd : MonoBehaviour {
 
-
+	public AudioSource mySource;
+	public Text myText;
 	
 	// Update is called once per frame
 	void OnTriggerEnter (object other) {
-		GetComponent<ParticleSystem>().gameObject.SetActive (true);
+		myText.text = "Success is a societal expectation not the norm";
+		mySource.Stop ();
 	}
 }
